7893e4936e7fa828490a1ff572bfccaf9eafaaca
Ahmad Ayman <mongez.me@gmail.com>
2021-04-16 00:32:49 +0200

    test api calls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useEffect } from "react";
+import * as BookAPI from "./services/BooksAPI.js";
 
 function App() {
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      const result = await BookAPI.getAll();
+      console.log(result);
+    };
+    const searchReact = async () => {
+      const result = await BookAPI.search("React");
+      console.log(result);
+    };
+
+    // this will update the state of the book
+    // and return object with 3 arrays for currently reading, read, want to read
+    const updateProReact = async () => {
+      const proReactBook = await BookAPI.get("PKpPCwAAQBAJ");
+      console.log(proReactBook);
+      const result = await BookAPI.update(proReactBook, "currentlyReading");
+      console.log(result);
+    };
+
+    updateProReact();
+    searchReact();
+    fetchAllBooks();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
